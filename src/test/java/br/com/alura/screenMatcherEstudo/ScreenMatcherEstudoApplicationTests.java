9145a2c3ffc871e56ba1a687f750b2b233b1d60a
Marina-Gouveia <marinagouveia420@gmail.com>
2024-01-29 04:50:49 -0300

    Inicio projeto Spring

---
+++ src/test/java/br/com/alura/screenMatcherEstudo/ScreenMatcherEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenMatcherEstudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMatcherEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
