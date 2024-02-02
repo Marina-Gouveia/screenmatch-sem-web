070269c2010c4e63ac4bab4dfb4e7275d3672327
Marina-Gouveia <marinagouveia420@gmail.com>
2024-02-02 12:07:57 -0300

    Trabalhando com Streams, Lambdas e Estatisticas

--- src/main/java/br/com/alura/screenMatcherEstudo/ScreenMatcherEstudoApplication.java
+++ src/main/java/br/com/alura/screenMatcherEstudo/ScreenMatcherEstudoApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenMatcherEstudo;
 
-import br.com.alura.screenMatcherEstudo.model.DadosSerie;
-import br.com.alura.screenMatcherEstudo.service.ConsumoApi;
-import br.com.alura.screenMatcherEstudo.service.ConverteDados;
+import br.com.alura.screenMatcherEstudo.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,15 +14,7 @@ public class ScreenMatcherEstudoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-
-
-
+		Principal principal = new Principal();
+		principal.exibMenu();
 	}
 }
